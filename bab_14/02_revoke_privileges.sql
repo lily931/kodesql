c5eb9ef1e27a5f1ed568e189eb3e485dbc641062
lily931 <lily522@guru.smk.belajar.id>
2025-07-28 04:54:26 +0700

    Delete bab_14/02_revoke_privileges.sql

--- bab_14/02_revoke_privileges.sql
+++
@@ -1,5 +0,0 @@
--- Bab 14: Keamanan dan Otorisasi
--- Contoh mencabut hak akses
-
--- Mencabut hak DELETE pada tabel MAHASISWA dari pengguna 'staf_akademik'
-REVOKE DELETE ON MAHASISWA FROM staf_akademik;
