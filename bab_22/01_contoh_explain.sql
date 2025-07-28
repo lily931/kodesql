e918718316d962104f9fd871043dac3f3d1b5954
lily931 <lily522@guru.smk.belajar.id>
2025-07-28 22:43:18 +0700

    Update 01_contoh_explain.sql

--- bab_22/01_contoh_explain.sql
+++ bab_22/01_contoh_explain.sql
@@ -1,11 +1,9 @@
 -- Bab 22: Pengantar Optimisasi Kueri
--- Menjalankan EXPLAIN pada kueri sebelum dan sesudah indeks dibuat.
-
 -- 1. Analisis sebelum ada indeks (kemungkinan besar akan menghasilkan 'Seq Scan')
-EXPLAIN SELECT * FROM MAHASISWA WHERE Nama_Lengkap = 'Budi Santoso';
+EXPLAIN SELECT * FROM MAHASISWA WHERE Nama_Mahasiswa = 'Budi Santoso';
 
 -- 2. Membuat indeks
-CREATE INDEX idx_mahasiswa_nama ON MAHASISWA (Nama_Lengkap);
+CREATE INDEX idx_mahasiswa_nama1 ON mahasiswa (Nama_Mahasiswa(100));
 
 -- 3. Analisis setelah indeks dibuat (seharusnya menghasilkan 'Index Scan')
-EXPLAIN SELECT * FROM MAHASISWA WHERE Nama_Lengkap = 'Budi Santoso';
+EXPLAIN SELECT * FROM MAHASISWA WHERE Nama_Mahasiswa = 'Budi Santoso';
