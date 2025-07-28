5966227ee317d3f60adec6dfb178298c47fa43b6
lily931 <lily522@guru.smk.belajar.id>
2025-07-28 17:40:42 +0700

    Update 01_contoh_indeks.sql

--- bab_21/01_contoh_indeks.sql
+++ bab_21/01_contoh_indeks.sql
@@ -1,4 +1,5 @@
 -- Bab 21: Indeks dan Peningkatan Kinerja
 -- Membuat indeks pada kolom Nama_Lengkap untuk mempercepat pencarian.
 -- (Gunakan tabel MAHASISWA dari skrip setup Bab 16 atau Bab 19)
-CREATE INDEX idx_mahasiswa_nama ON MAHASISWA (Nama_Lengkap);
+
+CREATE INDEX idx_mahasiswa_nama ON MAHASISWA (Nama_Mahasiswa(100));
